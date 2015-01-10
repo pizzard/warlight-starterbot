4b0e7dc960aa50de167df0ab478a453f43018e94
Martin Beeger <martin@beeger.name>
2015-01-10 13:23:12 +0100

    added main.h to repository

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// tools
+#include "tools/noncopyable.hpp"
+
+/**
+ * This file contains gereral configuration and thing whoch should be in the global scope
+ */
+enum Player {
+	ME,
+	ENEMY,
+	NEUTRAL
+};
+
+#endif
